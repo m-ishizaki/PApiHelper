cd15ba0c4fcf28707b10c5129891539648001956
m ishizaki <love.ms.is12t@hotmail.co.jp>
2018-12-13 21:12:15 +0900

    RequestData に使えそうな条件のプロパティを追加

--- src/PApiHelper/PApiHelper/Models/PGetApiRequestData.cs
+++ src/PApiHelper/PApiHelper/Models/PGetApiRequestData.cs
@@ -7,5 +7,11 @@ namespace PApiHelper.Models
     public class PGetApiRequestData
     {
         public Dictionary<string, string> ColumnFilterHash { get; } = new Dictionary<string, string>();
+        public bool? Incomplete { get; set; }
+        public bool? Own { get; set; }
+        public bool? NearCompletionTime { get; set; }
+        public bool? Delay { get; set; }
+        public bool? Overdue { get; set; }
+        public Dictionary<string, int> ColumnSorterHash { get; } = new Dictionary<string, int>();
     }
 }
